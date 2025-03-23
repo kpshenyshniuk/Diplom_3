1ef7efb382d3fb31d20769daa4727900ec9546e8
kpshenyshniuk <k.pshenyshniuk@abcmobile.com>
2025-03-23 10:56:10 +0100

    теперь точно последний

--- tests/test_restore_password_page.py
+++ tests/test_restore_password_page.py
@@ -15,14 +15,13 @@ class TestRestorePasswordPage:
         login_page.open_login_page()
         login_page.click_button_restore_password()
 
-        assert reset_page.get_current_url == Links.link_forgot_password_page
+        assert reset_page.get_current_url() == Links.link_forgot_password_page
         assert reset_page.find_header_restore_password()
 
     @allure.title("Тест восстановление пароля")
     def test_restore_password(self, driver, create_user):
         login_page = LoginPage(driver)
         reset_page = ResetPasswordPage(driver)
-        main_page = MainPage(driver)
         user_data, password = create_user
         login_page.open_login_page()
         login_page.click_button_restore_password()
@@ -30,7 +29,7 @@ class TestRestorePasswordPage:
         reset_page.click_on_button_restore_password_restore_page()
         reset_page.wait_text_in_locator_overlay_reset_password_page()
 
-        assert reset_page.get_current_url == Links.reset_password_link
+        assert reset_page.get_current_url() == Links.reset_password_link
         assert reset_page.find_input_new_password_reset_password_page()
         assert reset_page.find_input_code_reset_password_page()
 
